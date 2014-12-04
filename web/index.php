5da73b5f77b9c59ceae3dd218c169554b4a67172
Heroku User <piquadrat@gmail.com>
2014-12-04 17:23:16 +0000

    Import source code

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$organization_id = getenv('OPBEAT_ORGANIZATION_ID')
+$application_id = getenv('OPBEAT_APP_ID')
+$secret_token = getenv('OPBEAT_SECRET_TOKEN')
+
+if ($organization_id && $application_id && $secret_token){
+  $client = new Opbeat_Client($organization_id, $application_id, $secret_token);
+  $handler = new Opbeat_Handler();
+  $handler->addClient($client);
+  $handler->registerErrorHandler();
+  $handler->registerExceptionHandler();
+}
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
+});
+
+$app->get('/error', function() use($app) {
+    throw new Exception('Oh.');
+});
+
+$app->run();
+
+?>

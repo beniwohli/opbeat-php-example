2ad7373cdf83bce054fe99672691bb35ccee24e2
Benjamin Wohlwend <piquadrat@gmail.com>
2014-12-04 19:22:13 +0100

    try to set default endpoint

--- web/index.php
+++ web/index.php
@@ -6,8 +6,12 @@ $organization_id = getenv('OPBEAT_ORGANIZATION_ID');
 $application_id = getenv('OPBEAT_APP_ID');
 $secret_token = getenv('OPBEAT_SECRET_TOKEN');
 
+class New_Opbeat_Client extends Opbeat_Client {
+  protected $default_endpoint = 'https://opbeat.com/api/v1/';
+}
+
 if ($organization_id && $application_id && $secret_token){
-  $client = new Opbeat_Client($organization_id, $application_id, $secret_token);
+  $client = new New_Opbeat_Client($organization_id, $application_id, $secret_token);
   $handler = new Opbeat_Handler();
   $handler->addClient($client);
   $handler->registerErrorHandler();
